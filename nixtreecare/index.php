c6241697564daa763b5a1039ebdd5969f25575c7
Jackie Rubly <jrubly@mail.greenriver.edu>
2017-11-02 14:52:59 -0700

    Add files via upload

---
+++ nixtreecare/index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Nix Tree Services PDF Creator</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3mobile.css">
+		<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-amber.css">
+		
+    </head>
+    <body>
+        <header align = 'center'>
+            <img src="/test/imgs/logo.jpg" width = "50" width = "50">
+            <h1>
+                Nix Tree Services
+            </h1>
+        </header>        
+        <br>
+        <div id = "input">
+			<div class="six columns">
+				
+				<label for="exampleRecipientInput">Reason for contacting</label>
+					<select class="u-full-width" id="exampleRecipientInput">
+					<option value="Option 1">Questions</option>
+					<option value="Option 2">Admiration</option>
+					<option value="Option 3">Can I get your number?</option>
+					</select>
+	  
+                <p>
+                    <label>Customer Name:</label>
+                    <br>
+                    <input type='text' name='custName' />
+                </p>
+                <p>
+                    <label>Address:</label>
+                    <input type='text' name='street' />
+                </p>
+                <p>
+                    <label>City:</label>
+                    <input type='text' name='city' />
+                </p>
+                <p>
+                    <label>Phone/ Ext:</label>
+                    <input type='text' name='phone' />
+                </p>
+                <p>
+                    <label>Email:</label>
+                    <input type='text' name='email' />
+                </p>
+				  <input class="button-primary" type="submit" value="Submit">
+
+            </form>
+        </div>
+    </body>
+</html>
